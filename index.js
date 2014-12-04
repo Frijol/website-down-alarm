6559d375396ddf1fd923ec4323b2380b3ab4510e
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2014-12-04 14:36:41 -0800

    mostly working, but only pings twice

--- index.js
+++ index.js
@@ -1,30 +1,125 @@
-/*********************************************
-This relay module demo toggles both relay
-channels every two seconds, logging the new
-values to the console upon latching.
-*********************************************/
+// SET THESE VARIABLES
+var siteToMonitor = 'http://www.google.com/adsfasdf';
+var wifiSettings = {
+  ssid: "technicallyWifi", // Your wifi network
+  password: "scriptstick", // Your wifi password
+  timeout: 40
+};
 
+// Require node libraries
 var tessel = require('tessel');
 var relaylib = require('relay-mono');
+var wifi = require('wifi-cc3000');
+var http = require('http');
 
-var relay = relaylib.use(tessel.port['A']);  
+// Local vars
+var timeouts = 0; // Counts wifi timeouts
 
-// Wait for the module to connect
+// Connect hardware
+var relay = relaylib.use(tessel.port['A']);
 relay.on('ready', function relayReady () {
-  console.log('Ready! Toggling relays...');
-  setInterval(function toggle() {
-    // Toggle relay channel 1
-    relay.toggle(1, function toggleOneResult(err) {
-      if (err) console.log("Err toggling 1", err);
-    });
-    // Toggle relay channel 2
-    relay.toggle(2, function toggleTwoResult(err) {
-      if (err) console.log("Err toggling 2", err);
-    });
-  }, 2000); // Every 2 seconds (2000ms)
+  // Connect wifi
+  checkConnection();
 });
 
-// When a relay channel is set, it emits the 'latch' event
-relay.on('latch', function(channel, value) {
-  console.log('latch on relay channel ' + channel + ' switched to', value);
+// Main function: runs when everything is ready
+function main() {
+  console.log('Ready! Pinging website...');
+  // Check status of website
+  setInterval(function checkSite() {
+    http.get(siteToMonitor, function(res) {
+      console.log("Got response: " + res.statusCode);
+      // Check for bad status
+      if(res.statusCode == 200) {
+        relay.turnOff(1, function (err) {
+          if(err) {
+            console.log(err);
+            systemError();
+          }
+        });
+      } else {
+        relay.turnOn(1, function (err) {
+          if(err) {
+            console.log(err);
+            systemError();
+          }
+        });
+      }
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+      systemError();
+    });
+  }, 2000); // Every 10 seconds
+}
+
+function checkConnection () {
+  if (wifi.isConnected()) {
+    console.log('Connected.');
+    // When wifi is connected, run the main function
+    main();
+  } else {
+    console.log('Connecting...');
+    wifi.connect(wifiSettings, function (err, res) {
+      if(err) {
+        console.log('Error connecting:', err);
+        systemError();
+      }
+      // Try again
+      checkConnection();
+    });
+  }
+}
+
+// Automatic wifi handling
+wifi.on('disconnect', function () {
+  console.log('Disconnected.');
+  systemError();
+  checkConnection();
 });
+
+wifi.on('timeout', function(err){
+  // Tried to connect but couldn't; retry
+  console.log("Timed out connecting to wifi");
+  timeouts++;
+  if (timeouts > 2) {
+    // Reset the wifi chip if we've timed out too many times
+    powerCycle();
+  } else {
+    // Try to reconnect
+    checkConnection();
+  }
+});
+
+wifi.on('error', function(err){
+  // One of the following happened
+  // 1. tried to disconnect while not connected
+  // 2. tried to disconnect while in the middle of trying to connect
+  // 3. tried to initialize a connection without first waiting for a timeout or a disconnect
+  console.log("error emitted", err);
+  systemError();
+});
+
+// Reset the wifi chip progammatically
+function powerCycle(){
+  // When the wifi chip resets, it will automatically try to reconnect to the last saved network
+  console.log('Cycling power...');
+  wifi.reset(function(){
+    timeouts = 0; // Reset timeouts
+    console.log("Done power cycling, waiting to reconnect...");
+    // Give it some time to auto reconnect
+    setTimeout(function(){
+      if (!wifi.isConnected()) {
+        // Try to reconnect
+        connect();
+      }
+      }, 20 *1000); // 20 second wait
+  });
+}
+
+// Whenever this system isn't working, flash the light
+function systemError() {
+  relay.toggle(1);
+  setTimeout(function () {
+    relay.toggle(1);
+  }, 2000);
+}
